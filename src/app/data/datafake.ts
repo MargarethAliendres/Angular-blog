5934d8ccb579094ba6a1f3000bfaa46f8ff773e9
Margareth <margareth.aliendres@gmail.com>
2023-04-26 20:00:29 -0300

    firstComiit

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo filme",
+    "description": "olabbbbb",
+    "photo__cover": "https://tse4.mm.bing.net/th?id=OIP.98qY8YNHD_UsLJ_AE0CRcAHaI4&pid=Api&P=0"
+  },
+
+  { "id": "2",
+  "title": "Novo filme",
+  "description": "olabbbbb",
+  "photo__cover": "https://tse4.mm.bing.net/th?id=OIP.98qY8YNHD_UsLJ_AE0CRcAHaI4&pid=Api&P=0"
+  },
+  {
+    "id": "3",
+    "title": "Novo filme",
+    "description": "olabbbbb",
+    "photo__cover": "https://tse4.mm.bing.net/th?id=OIP.98qY8YNHD_UsLJ_AE0CRcAHaI4&pid=Api&P=0"
+  },
+  {
+    "id": "4",
+    "title": "Novo filme",
+    "description": "olabbbbb",
+    "photo__cover": "https://tse4.mm.bing.net/th?id=OIP.98qY8YNHD_UsLJ_AE0CRcAHaI4&pid=Api&P=0"
+  }
+
+]
+
